906d229b943963bef5adc69c28a57bb84287cf44
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-03-25 21:24:40 +0700

    iii

--- app/Http/Controllers/eloCustController.php
+++ app/Http/Controllers/eloCustController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\eloCust;
 
 class eloCustController extends Controller
 {
-    //
+    public function index() {
+        // mengambil data konsumen dengan eloquent ORM
+        $konsumen = eloCust::all();
+        if($konsumen->count() > 0) { 
+        //mengirim data konsumen ke view index
+    	    return view('indexCustomer',['konsumen' => $konsumen]);
+        } 
+        else { 
+            return view('indexCustomer',['konsumen' => array()]);
+        }
+    }
 }
--- app/Models/eloCust.php
+++ app/Models/eloCust.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class eloCust extends Model
 {
     use HasFactory;
+    // table
+    protected $table = 'customer';
+    // primary key
+    protected $primaryKey = 'id_table_customer ';
+    // coloumn table
+    protected $fillable = 
+    [
+        'nama_customer',
+        'bod_customer',
+        'phone_customer',
+        'status_delete'
+    ];
+    public function alamatRelation()
+    {
+        return $this->hasMany(Alamat::class,'id_customers'); 
+    }
+    public function RekeningRelation()
+    {
+        return $this->hasMany(Rekening::class,'id_customers');
+    }
 }
