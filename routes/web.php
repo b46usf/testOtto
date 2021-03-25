889c97a82b58212e92fdcc4622674e4fb23d7889
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-03-25 21:26:58 +0700

    iii

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// route pages customer
+// route pages customer with model query builder
 // Route::get('/customer', 'App\Http\Controllers\customerController@index');
 // Route::get('/customer/input', 'App\Http\Controllers\customerController@create');
 // Route::get('/customer/show', 'App\Http\Controllers\customerController@create');
@@ -24,8 +24,17 @@ Route::get('/', function () {
 // Route::post('/customer/delete', 'App\Http\Controllers\customerController@delete');
 // Route::post('/customer/store', 'App\Http\Controllers\customerController@store');
 // Route::post('/customer/table', 'App\Http\Controllers\customerController@show');
-// route konsumen
-Route::get('/customer', 'App\Http\Controllers\konsumenController@index');
+// route konsumen without model
+// Route::get('/customer', 'App\Http\Controllers\konsumenController@index');
+// Route::get('/customer/input', 'App\Http\Controllers\konsumenController@create');
+// //Route::get('/customer/show', 'App\Http\Controllers\konsumenController@show');
+// Route::get('/customer/show/{id}', 'App\Http\Controllers\konsumenController@show');
+// Route::post('/customer/edit', 'App\Http\Controllers\konsumenController@edit');
+// Route::post('/customer/update', 'App\Http\Controllers\konsumenController@update');
+// Route::post('/customer/delete', 'App\Http\Controllers\konsumenController@delete');
+// Route::post('/customer/store', 'App\Http\Controllers\konsumenController@store');
+// route pages customer with eloquent model
+Route::get('/customer', 'App\Http\Controllers\eloCustController@index');
 Route::get('/customer/input', 'App\Http\Controllers\konsumenController@create');
 //Route::get('/customer/show', 'App\Http\Controllers\konsumenController@show');
 Route::get('/customer/show/{id}', 'App\Http\Controllers\konsumenController@show');
