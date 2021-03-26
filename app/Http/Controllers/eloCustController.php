9d713fb9fcffcbf05d91934b1c1ac4b44c7865ec
ainandoo <ainandoo@example.com>
2021-03-26 15:25:05 +0700

    iii

--- app/Http/Controllers/eloCustController.php
+++ app/Http/Controllers/eloCustController.php
@@ -30,6 +30,8 @@ class eloCustController extends Controller
                 }
             }
         }
+        //echo "<pre>";
+        //print_r($object);
         if($konsumen->count() > 0) { 
         //mengirim data konsumen ke view index
     	    return view('indexCustomer',['konsumen' => json_decode(json_encode($dtkonsumen),FALSE)]);
@@ -43,10 +45,37 @@ class eloCustController extends Controller
     }
     public function show($id) {
         // mengambil data konsumen by id dengan eloquent ORM
-        $konsumen = eloCust::where('uniqID_Customer',$id)->get();dd($konsumen);
+        $konsumen   = eloCust::with('eloAdr','eloRek','eloCustImg')->where('uniqID_Customer',$id)->get();
+        //$konsumen   = eloCust::where('uniqID_Customer',$id)->get();
+        // mengubah ke array
+        $data       = $konsumen->toArray();
+        // mengubah ke objek
+        $object     = json_decode(json_encode($data),FALSE);
+        // passing data
+        foreach($object as $key) {
+            foreach($key->elo_adr as $kAdr) {
+                foreach($key->elo_rek as $kRek) {
+                    foreach($key->elo_cust_img as $kImg) {
+                        $dtkonsumen[]       =   array(
+                            'uniqID_Customer' 	=> $key->uniqID_Customer,
+                            'email_customer' 	=> $key->email_customer,
+                            'nama_customer' 	=> $key->nama_customer,
+                            'bod_customer'      => $key->bod_customer,
+                            'phone_customer' 	=> $key->phone_customer,
+                            'alamat'            => $kAdr->alamat,
+                            'bank_rekening'     => $kRek->bank_rekening,
+                            'nomor_rekening'    => $kRek->nomor_rekening,
+                            'file_location'     => $kImg->file_location,
+                            'file_image'        => $kImg->file_image
+                        );
+                    }
+                }
+            }
+        }
+        //dd($dtkonsumen);
         if($konsumen->count() > 0) { 
         //mengirim data konsumen ke view input
-            return view('formCustomer',['konsumen' => $konsumen]);
+            return view('formCustomer',['konsumen' => json_decode(json_encode($dtkonsumen),FALSE)]);
         } 
         else { 
             return view('formCustomer',['konsumen' => array()]);
