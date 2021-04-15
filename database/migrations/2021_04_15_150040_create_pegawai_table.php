f16f88061ede4cc3bf5f6f5a9adb7d74f3c7cba7
Triyana Group <Triyana Group@DESKTOP-DJTTJJ9>
2021-04-15 22:18:19 +0700

    iii

---
+++ database/migrations/2021_04_15_150040_create_pegawai_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nomor_induk');
+            $table->string('nama');
+            $table->text('alamat');
+            $table->date('tanggal_lahir');
+            $table->date('tanggal_gabung');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+}
